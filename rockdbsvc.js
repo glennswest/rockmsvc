fcf2c79e3ef8b4796c6dcf76741feeeb40b11a5e
Glenn West <glennswest@neuralcloudcomputing.com>
2016-12-18 11:18:52 +0800

    Add rocks

---
+++ rockdbsvc.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+var plugins = require('restify-plugins');
+
+const winston = require('winston');
+
+winston.level = 'debug';
+winston.info(require.main.filename);
+
+const server = restify.createServer({
+  name: 'myapp',
+  version: '1.0.0'
+});
+server.use(plugins.acceptParser(server.acceptable));
+server.use(plugins.queryParser());
+server.use(plugins.bodyParser());
+ 
+ 
+server.get('/echo/:name', function (req, res, next) {
+  res.send(req.params);
+  return next();
+});
+ 
+server.listen(8080, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
