75ec4ffa8547a84fda66ba5db8ab1e7b265b3f31
glenn.west <glennswest@neuralcloudcomputing.com>
2016-12-19 02:21:22 +0000

    Fix build.sh to auto setup .bashrc

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
-    sudo yum install git
-    git config user.name "glenn.west"
-    git config user.email "glennswest@neuralcloudcomputing.com"
-    sudo yum install gcc gcc-c++
-    curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
-   nvm install v6.9.2
+sudo yum install git
+git config user.name "glenn.west"
+git config user.email "glennswest@neuralcloudcomputing.com"
+sudo yum install gcc gcc-c++
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
+echo <<'EOF' >> .bashrc
+export NVM_DIR="/home/vagrant/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+EOF
+source .bashrc
+nvm install v6.9.2
